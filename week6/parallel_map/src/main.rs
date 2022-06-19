838949e4cfafb33bbf61adba9ad3529202f716c2
clundro <clusternlh@gmail.com>
2022-06-20 04:44:06 +0800

    [fish] finish wee6.
    Signed-off-by: clundro <clusternlh@gmail.com>

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -1,3 +1,4 @@
+use core::num;
 use crossbeam_channel;
 use std::{thread, time};
 
@@ -8,7 +9,41 @@ where
     U: Send + 'static + Default,
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
-    // TODO: implement parallel map!
+    output_vec.resize_with(input_vec.len(), Default::default);
+    let mut thread = Vec::new();
+
+    let (in_sender, in_receiver) = crossbeam_channel::unbounded();
+    let (out_sender, out_receiver) = crossbeam_channel::unbounded();
+    for _ in 0..num_threads {
+        let in_receiver = in_receiver.clone();
+        let out_sender = out_sender.clone();
+        thread.push(thread::spawn(move || {
+            while let Ok(next_pair) = in_receiver.recv() {
+                // receive the value, handle it, then send it back
+                let (idx, value) = next_pair;
+                out_sender
+                    .send((idx, f(value)))
+                    .expect("there is no receiver");
+            }
+        }));
+    }
+    let len = input_vec.len();
+    for i in 0..len {
+        in_sender
+            .send((len - i - 1, input_vec.pop().unwrap()))
+            .expect("there is no receiver");
+    }
+
+    drop(in_sender);
+    drop(out_sender);
+    while let Ok(result_pair) = out_receiver.recv() {
+        let (idx, value) = result_pair;
+        // println!("main: {}", idx);
+        output_vec[idx] = value;
+    }
+    for t in thread {
+        t.join().expect("Panic occurred in thread");
+    }
     output_vec
 }
 
